d77a1f9cbdfe62571f7afad8fcb73ace7c6abb3f
Chris Garrett <z@zworkbench.com>
2011-03-10 13:52:03 -0500

    Added XCode project

---
+++ cocoa-bucks/cocoa-bucks/CBucks.h
@@ -0,0 +1,8 @@
+//
+//  CBucks.h
+//  cocoa-bucks
+//
+//  Created by Christopher Garrett on 3/10/11.
+//  Copyright 2011 ZWorkbench, Inc. All rights reserved.
+//
+#import "NSString+CBucks.h"
---
+++ cocoa-bucks/cocoa-bucksTests/cocoa_bucksTests.h
@@ -0,0 +1,17 @@
+//
+//  cocoa_bucksTests.h
+//  cocoa-bucksTests
+//
+//  Created by Christopher Garrett on 3/10/11.
+//  Copyright 2011 ZWorkbench, Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface cocoa_bucksTests : SenTestCase {
+@private
+    
+}
+
+@end
